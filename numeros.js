1e498548e9a4a7130ce282d73c5e2e58e5d9b304
Sobri <57077187+constSobri@users.noreply.github.com>
2023-02-12 20:38:56 -0600

    Creo mi codigo completo
    Utilizable para checks

---
+++ numeros.js
@@ -0,0 +1,15 @@
+let numeroIf = -50;
+
+if (numeroIf == 0) {
+  console.log("es cero");
+}
+else if (numeroIf > 0) {
+  console.log("es positivo");
+}
+else if (numeroIf < 0) {
+  console.log ("es negativo");
+}
+
+else {
+  console.log ("Seguro que es un numero?");
+}
