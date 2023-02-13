ba9083d0f2ed474b8c0c66e928d43b3e22b66d42
Sobri <57077187+constSobri@users.noreply.github.com>
2023-02-12 20:55:45 -0600

    Nuevo codigo
    Se puede utilizar para verificar datos complejos.

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+// Este codigo lo que hace es que toma la variable numeroFor para darle el valor a otra variable "i" (se que no es necesario pero asi lo quise y esta bueno para temas de practica), luego si "i" es menor o igual 3 entonces tendra que sumarse por 1 hasta que termine. Todo mientras se imprime en pantalla.
+
+let numeroFor = 0;
+
+for (i = numeroFor; 3 >= i; i++) {
+  console.log(i);
+}
